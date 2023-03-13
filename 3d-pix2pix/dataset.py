636d0b6e2f98e823c12cdbb181b8786755c26faa
a-saraf <a-saraf@github.com>
2023-03-13 18:29:51 +0530

    basic

--- 3d-pix2pix/dataset.py
+++ 3d-pix2pix/dataset.py
@@ -1,5 +1,6 @@
 import nibabel as nib
 import numpy as np
+import os
 
 path_file = './BraTSReg_002_00_0000_flair.nii.gz'
 
@@ -7,3 +8,12 @@ t1 = nib.load(path_file)
 t1 = t1.get_fdata()
 
 print(t1.shape)
+
+def make_dataset(dir):
+    images = []
+    
+    for file_name in sorted(os.listdir(dir)):
+        file_path = dir + '/' + file_name
+        if(file_name.endswith('nii.gz')):
+            images.append(file_name)
+    return images
