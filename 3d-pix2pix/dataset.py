44325a0335a7aafc136735668f3620ac00a852f8
a-saraf <a-saraf@github.com>
2023-03-15 18:05:30 +0530

    dataset done

--- 3d-pix2pix/dataset.py
+++ 3d-pix2pix/dataset.py
@@ -1,13 +1,9 @@
 import nibabel as nib
-import numpy as np
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
 import os
 
-path_file = './BraTSReg_002_00_0000_flair.nii.gz'
-
-t1 = nib.load(path_file)
-t1 = t1.get_fdata()
-
-print(t1.shape)
+path_file = '../DATA/00_Train/'
 
 def make_dataset(dir):
     images = []
@@ -17,3 +13,24 @@ def make_dataset(dir):
         if(file_name.endswith('nii.gz')):
             images.append(file_path)
     return images
+
+class BaseDataset(Dataset):
+    def __init__(self, dir, transform = None):
+        self.dir = dir
+        self.images = make_dataset(dir)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        img = nib.load(self.images[index])
+        img = img.get_fdata()
+        if self.transform:
+            img = self.transform(img)
+        return img
+    
+def create_dataset(dir):
+    dataset = BaseDataset(dir = dir, transform = transforms.Compose([transforms.ToTensor()]))
+    dataloader = DataLoader(dataset)
+    return dataloader
--- 3d-pix2pix/train.py
+++ 3d-pix2pix/train.py
@@ -1,3 +1,9 @@
 import torch
 import numpy as np
-from .dataset import BaseDataset
+from dataset import create_dataset
+
+dir = '../DATA/00_Train/'
+
+dataset = create_dataset(dir)
+
+print(len(dataset))
