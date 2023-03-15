16c321aadcfa62af24fe0f348bc4cca5551d7048
a-saraf <a-saraf@github.com>
2023-03-15 18:08:25 +0530

    dataset done

--- 3d-pix2pix/train.py
+++ 3d-pix2pix/train.py
@@ -7,3 +7,11 @@ dir = '../DATA/00_Train/'
 dataset = create_dataset(dir)
 
 print(len(dataset))
+for test_images in dataset:
+    sample = test_images[0]
+    print(sample.shape)
+    print(sample)
+    break
+
+# 102 Images Dataloader
+# dim 155 240 240 3D image
